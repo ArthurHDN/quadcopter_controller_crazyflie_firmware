36b86ac80e2d90c51e334cb490e04ff1653507d5
Arthur Nunes <arthur.nunes@hexagon.com>
2023-11-25 20:28:08 -0300

    Modify firmware to disable tumbe check

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
+cd crazyflie-firmware
+git apply ../disable-tumble-check.diff
+cd -
+
 cd app_my_controller
 make clean && make &&  python3 -m cfloader flash build/cf2.bin stm32-fw -w radio://0/100/2M/E7E7E7E704
+cd -
+
+cd crazyflie-firmware
+git restore .
 cd -
